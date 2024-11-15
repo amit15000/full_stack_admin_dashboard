50a7600095480064208f3dc5dc4ca6b83aa802ca
Amit Kumar <15000amitkumar@gmail.com>
2024-11-15 21:33:02 +0530

    Theme Color

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import bodyParser from "bodyParser";
+import bodyParser from "body-parser";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import morgan from "morgan";
 //routes
-import clientRoutes from "./routes/client";
-import generalRoutes from "./routes/general";
-import salesRoutes from "./routes/sales";
-import managementRoutes from "./routes/management";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/client.js";
+import salesRoutes from "./routes/client.js";
+import managementRoutes from "./routes/client.js";
 
 dotenv.config();
 const app = express();
@@ -17,10 +17,23 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser());
 app.use(cors());
 
 app.use("/client", clientRoutes);
 app.use("/sales", salesRoutes);
-app.use("/client", clientRoutes);
-app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database conneciton succeful");
+  })
+  .catch((err) => console.log(err))
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server PORT : ${PORT}`));
+  })
+  .catch((err) => {
+    console.log("Server failed :", err);
+  });
