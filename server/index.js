b93e37c15e286729b121e7fc17d4419a0b563b29
Amit Kumar <15000amitkumar@gmail.com>
2024-11-15 09:47:27 +0530

    ADDED middlewares

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import bodyParser from "bodyParser";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import morgan from "morgan";
+//routes
+import clientRoutes from "./routes/client";
+import generalRoutes from "./routes/general";
+import salesRoutes from "./routes/sales";
+import managementRoutes from "./routes/management";
 
 dotenv.config();
 const app = express();
@@ -13,5 +18,9 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser());
+app.use(cors());
 
-app.use("/client");
+app.use("/client", clientRoutes);
+app.use("/sales", salesRoutes);
+app.use("/client", clientRoutes);
+app.use("/client", clientRoutes);
